f21ea243cd849e821ec9c51bff2a36ba741a98ad
YashBhatt01 <159613984+YashBhatt01@users.noreply.github.com>
2024-03-13 16:39:41 +0000

    variables discusion

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = "39504"
+let accountEmail = "yasgbv==@gmail.com"
+var accountPassword = "93-5kd43!"
+accountCity = "Dehradun"
+let accountState;
+
+// accountId = "34444" // Not allowed Because It is a constant variable
+accountId = "11111"
+console.log(accountId)
+
+//Prefer not to use VAR 
+//because of issue in scope block and functional scope
+
+//We can Print them All IN ONE By shortCut :-
+console.log([accountCity, accountId, accountPassword, accountEmail])
