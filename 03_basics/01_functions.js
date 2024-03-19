d23b95115d8c3376493877865bcecd3c86dc5e54
YashBhatt01 <159613984+YashBhatt01@users.noreply.github.com>
2024-03-19 18:51:19 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,24 @@
+// function sayMyName(){
+//     console.log("Yash");
+//     console.log("Bhatt");
+// }
+// // sayMyName()
+
+// function addTwoNumbers(number1, number2){
+// console.log(number1 + number2);
+}
+function addTwoNumbers(number1, number2){
+// let result = number1 + number2;
+// return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(6, "4")
+
+// console.log("Result: ", result);
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("Yash"))
