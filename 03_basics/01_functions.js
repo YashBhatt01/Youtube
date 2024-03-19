c1212dfafb6f53f039581169db7da4a508312ff0
YashBhatt01 <159613984+YashBhatt01@users.noreply.github.com>
2024-03-19 18:59:16 +0000

    Functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -6,19 +6,25 @@
 
 // function addTwoNumbers(number1, number2){
 // console.log(number1 + number2);
-}
+
 function addTwoNumbers(number1, number2){
 // let result = number1 + number2;
 // return result
-    return number1 + number2
+    return (number1 + number2)
 }
 
 const result = addTwoNumbers(6, "4")
 
 // console.log("Result: ", result);
 
-function loginUserMessage(username){
+function loginUserMessage(username = "sam"){
+    if(!username){
+    console.log("Please enter a user name");
+}
+else{
     return `${username} just logged in`
 }
+}
 
-console.log(loginUserMessage("Yash"))
+// console.log(loginUserMessage("Yash"));
+console.log(loginUserMessage("Yash"));  //OverRight
