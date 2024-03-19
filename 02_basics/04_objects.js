5589ebde1663964a03391298d7e18874eb62c767
YashBhatt01 <159613984+YashBhatt01@users.noreply.github.com>
2024-03-19 16:45:09 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -28,5 +28,41 @@ const obj2 = {
     4: "d"
 }
 // const obj3 = {obj1 , obj2}
-const obj3 = Object.assign({}, obj1,obj2)
-console.log(obj3)
+// const obj3 = Object.assign({}, obj1,obj2)
+// console.log(obj3)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const user = [
+    {
+        id: 1,
+        email: "yashbhatt@gmail.com"
+    },
+    {
+    },
+    {
+    }
+]
+
+user[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course  = {
+    coursename: "Js Master In One Week",
+    price: "00090000",
+    courseInstructor: "Yash"
+}
+
+// course.courseInstructor
+
+// const {courseInstructor: insss} = course
+
+console.log(insss);
